a4710f667ee7459bd8793c0434396aca29577eb5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-23 14:38:11 +0000

    No need to create badge person when a user is created
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@39664 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/subscribers.py
+++ src/nti/app/products/badges/subscribers.py
@@ -8,10 +8,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import time
-
 from zope import component
-from zope.lifecycleevent.interfaces import IObjectCreatedEvent
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 
 from nti.badges import interfaces as badges_interfaces
@@ -28,14 +25,6 @@ def _user_deleted(user, event):
     person = badges_interfaces.INTIPerson(user)
     for manager in get_user_badge_managers(user):
         manager.delete_person(person)
-
-@component.adapter(nti_interfaces.IUser, IObjectCreatedEvent)
-def _user_created(user, event):
-    ntiperson = badges_interfaces.INTIPerson(user)
-    for manager in get_user_badge_managers(user):
-        if not manager.person_exists(ntiperson):
-            ntiperson.createdTime = time.time()
-            manager.add_person(ntiperson)
 
 @component.adapter(IAfterDatabaseOpenedEvent)
 def _after_database_opened_listener(event):
