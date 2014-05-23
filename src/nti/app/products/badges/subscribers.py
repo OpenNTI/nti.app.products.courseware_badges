f406b458a9cb2c42d18a4606e226eff5aa80bb6d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-23 03:17:20 +0000

    after the database open add any registered tahrir issuer to all the tahrir badge manager
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@39646 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/subscribers.py
+++ src/nti/app/products/badges/subscribers.py
@@ -15,8 +15,11 @@ from zope.lifecycleevent.interfaces import IObjectCreatedEvent
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 
 from nti.badges import interfaces as badges_interfaces
+from nti.badges.tahrir import interfaces as tahrir_interfaces
 
 from nti.dataserver import interfaces as nti_interfaces
+
+from nti.processlifetime import IAfterDatabaseOpenedEvent
 
 from . import get_user_badge_managers
 
@@ -33,3 +36,12 @@ def _user_created(user, event):
         if not manager.person_exists(ntiperson):
             ntiperson.createdTime = time.time()
             manager.add_person(ntiperson)
+
+@component.adapter(IAfterDatabaseOpenedEvent)
+def _after_database_opened_listener(event):
+    import transaction
+    with transaction.manager:
+        for _, manager in component.getUtilitiesFor(tahrir_interfaces.ITahrirBadgeManager):
+            for _, issuer in component.getUtilitiesFor(tahrir_interfaces.IIssuer):
+                manager.add_issuer(issuer)
+            
