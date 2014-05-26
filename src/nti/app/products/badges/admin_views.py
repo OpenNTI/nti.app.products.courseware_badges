8d62a68f20b2cb0c8a6ac8f8805bd5e53f809d3c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-26 23:36:29 +0000

    add admin views to award, revoke, sync database
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.badges/trunk@39773 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/badges/admin_views.py
+++ src/nti/app/products/badges/admin_views.py
@@ -10,6 +10,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 import six
 import time
 import simplejson
@@ -17,6 +18,7 @@ import simplejson
 from zope import component
 
 from pyramid.view import view_config
+from pyramid import httpexceptions as hexc
 
 from nti.badges import interfaces as badges_interfaces
 
@@ -28,7 +30,10 @@ from nti.externalization.interfaces import LocatedExternalDict
 
 from nti.utils.maps import CaseInsensitiveDict
 
+from . import utils
 from . import views
+from . import interfaces
+from . import get_user_id
 from . import get_user_badge_managers
 
 def _make_min_max_btree_range(search_term):
@@ -88,5 +93,104 @@ def create_persons(request):
 
 	result = LocatedExternalDict()
 	result['Total'] = total
+	result['Elapsed'] = time.time() - now
+	return result
+
+@view_config(route_name='objects.generic.traversal',
+			 name='award',
+			 renderer='rest',
+			 request_method='POST',
+			 context=views.BadgeAdminPathAdapter,
+			 permission=nauth.ACT_MODERATE)
+def award(request):
+	values = readInput(request)
+	username = values.get('username', request.authenticated_userid)
+	user = User.get_user(username)
+	if user is None:
+		raise hexc.HTTPNotFound('User not found')
+	
+	badge = None
+	badge_name = values.get('badge', values.get('badge_name', values.get('badgeName')))
+	for manager in get_user_badge_managers(user):
+		badge = manager.get_badge(badge_name)
+		if badge is not None:
+			break
+
+	if badge is None:
+		raise hexc.HTTPNotFound('Badge not found')
+
+	predicate = interfaces.get_badge_predicate_for_user(user)
+	if not predicate(badge):
+		raise hexc.HTTPForbidden("Badge not authorized")
+
+	# add person if required
+	# an adapter must exists to convert the user to a person
+	manager.add_person(user)
+
+	# add assertion
+	uid = get_user_id(user)
+	manager.add_assertion(uid, badge_name)
+
+	return hexc.HTTPNoContent()
+
+@view_config(route_name='objects.generic.traversal',
+			 name='revoke',
+			 renderer='rest',
+			 request_method='POST',
+			 context=views.BadgeAdminPathAdapter,
+			 permission=nauth.ACT_MODERATE)
+def revoke(request):
+	values = readInput(request)
+	username = values.get('username', request.authenticated_userid)
+	user = User.get_user(username)
+	if user is None:
+		raise hexc.HTTPNotFound('User not found')
+
+	badge = None
+	badge_name = values.get('badge', values.get('badge_name', values.get('badgeName')))
+	for manager in get_user_badge_managers(user):
+		badge = manager.get_badge(badge_name)
+		if badge is not None:
+			break
+
+	if badge is None:
+		raise hexc.HTTPNotFound('Badge not found')
+
+	uid = get_user_id(user)
+	if manager.assertion_exists(uid, badge_name):
+		manager.remove_assertion(uid, badge_name)
+	else:
+		logger.debug('Assertion (%s,%s) not found', uid, badge_name)
+
+	return hexc.HTTPNoContent()
+
+@view_config(route_name='objects.generic.traversal',
+			 name='sync_db',
+			 renderer='rest',
+			 request_method='POST',
+			 context=views.BadgeAdminPathAdapter,
+			 permission=nauth.ACT_MODERATE)
+def sync_db(request):
+	values = readInput(request)
+
+	# get badge directory
+	directory = values.get('directory')
+	if not directory or not os.path.exists(directory) or not os.path.isdir(directory):
+		raise hexc.HTTPNotFound('Directory not found')
+
+	# get database name
+	dbname = values.get('dbid', values.get('db_id',
+					  values.get('dbname'), values.get('db_name')))
+
+	# verify object
+	verify = values.get('verify') or u''
+	verify = str(verify).lower() in ('1', 'true', 't', 'yes', 'y', 'on')
+
+	now = time.time()
+	# sync database
+	utils.sync.sync_db(directory, dbname, verify)
+
+	# return
+	result = LocatedExternalDict()
 	result['Elapsed'] = time.time() - now
 	return result
