929e6a7d31f19e11938381c1d33f18ceccdf2dbb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-05-27 20:43:09 +0000

    empty out
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@39851 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/subscribers.py
+++ src/nti/app/products/courseware_badges/subscribers.py
@@ -7,30 +7,3 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from zope import component
-from zope.lifecycleevent.interfaces import IObjectRemovedEvent
-
-from nti.badges import interfaces as badges_interfaces
-from nti.badges.tahrir import interfaces as tahrir_interfaces
-
-from nti.dataserver import interfaces as nti_interfaces
-
-from nti.processlifetime import IAfterDatabaseOpenedEvent
-
-from . import get_user_badge_managers
-
-@component.adapter(nti_interfaces.IUser, IObjectRemovedEvent)
-def _user_deleted(user, event):
-    person = badges_interfaces.INTIPerson(user)
-    for manager in get_user_badge_managers(user):
-        manager.delete_person(person)
-
-@component.adapter(IAfterDatabaseOpenedEvent)
-def _after_database_opened_listener(event):
-    import transaction
-    with transaction.manager:
-        for _, manager in component.getUtilitiesFor(tahrir_interfaces.ITahrirBadgeManager):
-            for _, issuer in component.getUtilitiesFor(tahrir_interfaces.IIssuer):
-                manager.add_issuer(issuer)
-            
--- src/nti/app/products/courseware_badges/tests/__init__.py
+++ src/nti/app/products/courseware_badges/tests/__init__.py
@@ -7,19 +7,11 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-import os
-import shutil
-import tempfile
-
 from zope import component
-
-from nti.badges.tahrir import manager
-from nti.badges import interfaces as badge_interfaces
 
 from nti.dataserver.tests.mock_dataserver import WithMockDS
 from nti.dataserver.tests.mock_dataserver import mock_db_trans
 
-from nti.app.products.badges.tests import generator
 from nti.app.testing.application_webtest import ApplicationTestLayer
 
 from nti.testing.layers import find_test
@@ -31,32 +23,22 @@ from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
 
 import zope.testing.cleanup
 
-def _change_ds_dir(cls):
-    cls.old_data_dir = os.getenv('DATASERVER_DATA_DIR')
-    cls.new_data_dir = tempfile.mkdtemp(dir="/tmp")
-    os.environ['DATASERVER_DATA_DIR'] = cls.new_data_dir
-
-def _restore_ds_dir(cls):
-    shutil.rmtree(cls.new_data_dir, True)
-    os.environ['DATASERVER_DATA_DIR'] = cls.old_data_dir or '/tmp'
-
 class SharedConfiguringTestLayer(ZopeComponentLayer,
                                  GCLayerMixin,
                                  ConfiguringLayerMixin,
                                  DSInjectorMixin):
 
-    set_up_packages = ('nti.dataserver', 'nti.badges', 'nti.app.products.badges')
+    set_up_packages = ('nti.dataserver',
+                       'nti.app.products.courseware_badges')
 
     @classmethod
     def setUp(cls):
         cls.setUpPackages()
-        _change_ds_dir(cls)
 
     @classmethod
     def tearDown(cls):
         cls.tearDownPackages()
         zope.testing.cleanup.cleanUp()
-        _restore_ds_dir(cls)
 
     @classmethod
     def testSetUp(cls, test=None):
@@ -68,26 +50,15 @@ class SharedConfiguringTestLayer(ZopeComponentLayer,
 
 import unittest
 
-class NTIBadgesTestCase(unittest.TestCase):
+class CourseBadgesTestCase(unittest.TestCase):
     layer = SharedConfiguringTestLayer
 
-sample_size = 5
-
-class NTIBadgesApplicationTestLayer(ApplicationTestLayer):
-
-    @classmethod
-    def _register_sample(cls):
-        import transaction
-        with transaction.manager:
-            bm = manager.create_badge_manager(defaultSQLite=True)
-            generator.generate_db(bm.db, sample_size, sample_size, sample_size)
-            component.provideUtility(bm, badge_interfaces.IBadgeManager, "sample")
+class CourseBadgesApplicationTestLayer(ApplicationTestLayer):
 
     @classmethod
     def setUp(cls):
-        _change_ds_dir(cls)
-        cls._register_sample()
+        pass
 
     @classmethod
     def tearDown(cls):
-        _restore_ds_dir(cls)
+        pass
