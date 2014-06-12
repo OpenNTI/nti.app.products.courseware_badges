d1b4899a8c33f7ad9892a8ca13353743ba5a2149
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-12 02:07:45 +0000

    remove unused
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@41162 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/subscribers.py
+++ src/nti/app/products/courseware_badges/subscribers.py
@@ -7,35 +7,3 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-import six
-import simplejson
-
-from zope import component
-from zope.lifecycleevent import IObjectAddedEvent
-
-from nti.contentlibrary.interfaces import ILegacyCourseConflatedContentPackage
-
-from . import interfaces
-
-@component.adapter(ILegacyCourseConflatedContentPackage, IObjectAddedEvent)
-def _content_package_registered(package, event):
-	if not package.courseInfoSrc:
-		return
-
-	info_json_string = package.read_contents_of_sibling_entry(package.courseInfoSrc)
-	if not info_json_string:
-		return
-
-	info_json_string = unicode(info_json_string, 'utf-8')
-	info_json_dict = simplejson.loads(info_json_string)
-
-	ntiid = package.ntiid
-	badge_map = component.getUtility(interfaces.ICourseBadgeMap)
-	badges = info_json_dict.get('badges') or info_json_dict.get('badge') or {}
-	if isinstance(badges, six.string_types):
-		badges = {badges:interfaces.COURSE_COMPLETION}
-	
-	for name, kind in badges.items():
-		kind = kind or interfaces.COURSE_COMPLETION
-		badge_map.add(ntiid, name, kind)
