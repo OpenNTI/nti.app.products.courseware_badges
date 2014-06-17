a26fb9eae1617f01754087ec1e4a753419bcac02
Carlos Sanchez <carlos.sanchez@nextthought.com>
2014-06-17 15:33:57 +0000

    use new nti.schema
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@41507 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/interfaces.py
+++ src/nti/app/products/courseware_badges/interfaces.py
@@ -13,63 +13,65 @@ from zope.interface.interface import taggedValue
 
 from nti.app.client_preferences.interfaces import TAG_EXTERNAL_PREFERENCE_GROUP
 
-from nti.utils import schema
+from nti.schema.field import Bool
+from nti.schema.field import Choice
+from nti.schema.field import ValidTextLine
 
 COURSE_COMPLETION = u'completion'
 COURSE_BADGE_TYPES = (COURSE_COMPLETION,)
 COURSE_BADGE_TYPES_VOCABULARY = \
-    vocabulary.SimpleVocabulary([vocabulary.SimpleTerm(_x) for _x in COURSE_BADGE_TYPES])
+	vocabulary.SimpleVocabulary([vocabulary.SimpleTerm(_x) for _x in COURSE_BADGE_TYPES])
 
 class ICourseBadge(interface.Interface):
-    name = schema.ValidTextLine(title="Badge name")
-    type = schema.Choice(vocabulary=COURSE_BADGE_TYPES_VOCABULARY,
-                         title='Badge type', required=True, default=COURSE_COMPLETION)
+	name = ValidTextLine(title="Badge name")
+	type = Choice(vocabulary=COURSE_BADGE_TYPES_VOCABULARY,
+				  title='Badge type', required=True, default=COURSE_COMPLETION)
 
 class ICourseBadgeMap(mapping.IReadMapping):
-    
-    def is_no_course(course):
-        """
-        check if the course is for no course
-        """
 
-    def mark_no_course(badge):
-        """
-        mark this badge name for a no course
-        """
+	def is_no_course(course):
+		"""
+		check if the course is for no course
+		"""
 
-    def mark(course):
-        """
-        mark a course this map
-        """
+	def mark_no_course(badge):
+		"""
+		mark this badge name for a no course
+		"""
 
-    def add(course, badge, kind=COURSE_COMPLETION):
-        """
-        register a badge name for specified course ntiid and badge kind
-        """
+	def mark(course):
+		"""
+		mark a course this map
+		"""
 
-    def get_badge_names(course):
-        """
-        return the badge names associated w/ the specified course ntiid
-        """
+	def add(course, badge, kind=COURSE_COMPLETION):
+		"""
+		register a badge name for specified course ntiid and badge kind
+		"""
 
-    def get_course_iden(badge):
-        """
-        return the course iden associated w/ the specfied badge name
-        """
-        
+	def get_badge_names(course):
+		"""
+		return the badge names associated w/ the specified course ntiid
+		"""
+
+	def get_course_iden(badge):
+		"""
+		return the course iden associated w/ the specfied badge name
+		"""
+
 class ICourseBadgeSettings(interface.Interface):
-    """
-    The root of the settings tree for badges
-    """
-    taggedValue(TAG_EXTERNAL_PREFERENCE_GROUP, 'write')
+	"""
+	The root of the settings tree for badges
+	"""
+	taggedValue(TAG_EXTERNAL_PREFERENCE_GROUP, 'write')
 
-    show_course_badges = schema.Bool(title="Enable/disable showing course badges",
-                                     description="Enable/disable showing course badges",
-                                     default=False)
+	show_course_badges = Bool(title="Enable/disable showing course badges",
+							  description="Enable/disable showing course badges",
+							  default=False)
 
 class ICourseBadgeCatalog(interface.Interface):
 
-    def iter_badges():
-        """
-        Return an iterable of badges for a course
-        """
+	def iter_badges():
+		"""
+		Return an iterable of badges for a course
+		"""
