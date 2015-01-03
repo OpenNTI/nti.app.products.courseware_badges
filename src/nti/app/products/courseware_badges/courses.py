0afefb69b5c7c6fb00d587ea6d28aa9e117f9aeb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-03 21:28:40 +0000

    change default to False
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@56409 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/courses.py
+++ src/nti/app/products/courseware_badges/courses.py
@@ -100,7 +100,7 @@ class _CoursePrincipalEarnableBadgeFilter(object):
 		pass
 
 	def allow_badge(self, user, badge):
-		result = True
+		result = False
 		entry = get_catalog_entry_for_badge(badge)
 		if entry is not None:
 			now = datetime.datetime.utcnow()
