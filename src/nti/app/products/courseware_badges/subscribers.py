8705b8eb63af05b821c567fbdd756d908e36b8c4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-01-16 01:33:55 +0000

    comment out cache builder subscriber
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@57759 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/subscribers.py
+++ src/nti/app/products/courseware_badges/subscribers.py
@@ -9,11 +9,7 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-import gevent
-
 from zope import component
-
-from nti.dataserver.interfaces import IDataserverTransactionRunner
 
 from nti.processlifetime import IApplicationTransactionOpenedEvent
 
@@ -21,12 +17,6 @@ from .interfaces import ICatalogEntryBadgeCache
 
 @component.adapter(IApplicationTransactionOpenedEvent)
 def _after_database_opened_listener(event):
-	def build_cache():
-		manager = component.getUtility(ICatalogEntryBadgeCache)
-		manager.build()
+	manager = component.getUtility(ICatalogEntryBadgeCache)
+	manager.build()
 	
-	def build_catalog_entry_badge_cache():
-		logger.info("Building catalog entry badge cache")
-		transaction_runner = component.getUtility(IDataserverTransactionRunner)
-		transaction_runner(build_cache)
-	gevent.spawn(build_catalog_entry_badge_cache)
