3ca424e5a53cce2ce609063df123d78240409d49
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-10-18 02:49:55 +0000

    minor opt
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.products.courseware_badges/trunk@98883 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/products/courseware_badges/views.py
+++ src/nti/app/products/courseware_badges/views.py
@@ -47,34 +47,31 @@ class CourseBadgesView(AbstractAuthenticatedView):
 	def __call__(self):
 		result = LocatedExternalDict()
 		result[ITEMS] = items = []
-
-		context = self.request.context
-		badge_catalog = ICourseBadgeCatalog(context, None)
-		badges = badge_catalog.iter_badges() if badge_catalog is not None else ()
-		items.extend(IBadgeClass(b) for b in badges)
-		result['Total'] = result['ItemCount'] = len(items)
-
-		result.__parent__ = context
+		result.__parent__ = self.context
 		result.__name__ = self.request.view_name
-		self.request.response.last_modified = context.lastModified
+		self.request.response.last_modified = self.context.lastModified
+		badge_catalog = ICourseBadgeCatalog(self.context, None)
+		if badge_catalog is not None:
+			items.extend(IBadgeClass(b) for b in badge_catalog.iter_badges())
+		result[TOTAL] = result[ITEM_COUNT] = len(items)
 		return result
 
 @view_config(route_name='objects.generic.traversal',
-			  context=IUser,
-			  request_method='GET',
-			  permission=nauth.ACT_READ,
-			  renderer='rest',
-			  name=VIEW_EARNED_COURSE_BADGES)
+			 context=IUser,
+			 request_method='GET',
+			 permission=nauth.ACT_READ,
+			 renderer='rest',
+			 name=VIEW_EARNED_COURSE_BADGES)
 class EarnedCourseBadgesView(AbstractAuthenticatedView):
 
 	def __call__(self):
 		result = LocatedExternalDict()
 		result[ITEMS] = items = []
-		context = self.request.context
-		if self.remoteUser.username == context.username or show_course_badges(context):
-			badges = get_earned_course_badges(context)
+		result.__parent__ = self.context
+		result.__name__ = self.request.view_name
+		if 		self.remoteUser.username == self.context.username \
+			or	show_course_badges(self.context):
+			badges = get_earned_course_badges(self.context)
 			items.extend(IBadgeClass(b) for b in badges)
 		result[TOTAL] = result[ITEM_COUNT] = len(items)
-		result.__parent__ = context
-		result.__name__ = self.request.view_name
 		return result
